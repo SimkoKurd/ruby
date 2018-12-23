ceead2c8113da26f534ee459c5a4b54e5f5e1ab8
Simko Kurdpoor <simko.kurdpoor@gmx.de>
2018-12-23 21:13:05 +0100

    Initialize repository

---
+++ lib/simko_palindrome.rb
@@ -0,0 +1,6 @@
+require "simko_palindrome/version"
+
+module SimkoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "simko_palindrome"
+
+require "minitest/autorun"
